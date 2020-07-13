33696241896cad5316e1e8992337639414154a82
jiangpingping <jiangpingping@cmss.chinamobile.com>
2020-07-13 23:42:04 +0800

    【title】 类加载器
    description: 类加载器确定对象唯一性测试

---
+++ naixue-chapter1-reflection/src/main/java/cn/itlemon/naixue/reflection/classload/ClInitTest.java
@@ -0,0 +1,15 @@
+package cn.itlemon.naixue.reflection.classload;
+
+/**
+ * @author jiangpingping
+ * @date 2020/7/13 22:28
+ */
+public class ClInitTest {
+
+    private static int index;
+
+    static {
+        index = 1000;
+    }
+
+}
